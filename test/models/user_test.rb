38d0f45190e62499547244c8c2beab8c921cf41f
m-vv <m_vv@outlook.com>
2017-02-06 12:44:46 +0200

    password middle

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "shiold be valid" do
@@ -62,6 +63,16 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "paswword should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "passord should nave a minimum length" do
+    @user.password = @user.password_confirmation = " " * 3
+    assert_not @user.valid?
   end
 
 
