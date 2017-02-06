326029796fbd1cd148669ff1458546aa5c128f27
m-vv <m_vv@outlook.com>
2017-02-06 10:45:39 +0200

    integrational test for link to test2 from front page

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,6 +11,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", test2_path
+
   end
 
 end
