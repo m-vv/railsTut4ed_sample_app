352e2cb8dc3d8975332582331d5cb0bc4627c3d3
m-vv <m_vv@outlook.com>
2017-03-06 01:14:17 +0200

    user login logic & test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   def hello
     render html: "hello, world from ROR"
   end
+
+  include SessionsHelper
+
 end
